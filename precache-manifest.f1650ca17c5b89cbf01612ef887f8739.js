67130eb35b391a12fce47ed720dce2afeeee1640
kevalriya <patelkeval123@gmail.com>
2019-10-02 08:46:27 -0400

    Updates

---
+++ precache-manifest.f1650ca17c5b89cbf01612ef887f8739.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73d928eef969d135a0aa4f514dddd6f6",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "6183ba2e77ca1e782cf0",
+    "url": "/Calculator/static/css/main.31f131ab.chunk.css"
+  },
+  {
+    "revision": "4715aba35e29e5a9dd36",
+    "url": "/Calculator/static/js/2.62630ba5.chunk.js"
+  },
+  {
+    "revision": "6183ba2e77ca1e782cf0",
+    "url": "/Calculator/static/js/main.8434c94a.chunk.js"
+  },
+  {
+    "revision": "da56ef84b29a9c3a00e1",
+    "url": "/Calculator/static/js/runtime-main.da01846c.js"
+  }
+]);
